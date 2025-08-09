5ec4d571c14ec4a65de3ed914960eacd3c39a04b
Zlatin Hristov <zlatin.hristov@softavis.com>
2025-08-09 22:50:49 +0300

    Configure Redis clustering and add `barryvdh/laravel-debugbar` to development dependencies.

--- app/config/database.php
+++ app/config/database.php
@@ -151,22 +151,73 @@ return [
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
+        'clusters' => [
+            'default' => [
+                [
+                    'url' => env('REDIS_URL'),
+//                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'host' => 'redis-node-1',
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
 
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+                ],
+                [
+                    'url' => env('REDIS_URL'),
+//                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'host' => 'redis-node-2',
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
+
+                ],
+                [
+                    'url' => env('REDIS_URL'),
+//                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'host' => 'redis-node-3',
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
+
+                ],
+            ],
+
+            'cache' => [
+                [
+                    'url' => env('REDIS_URL'),
+//                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'host' => 'redis-node-1',
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
+
+                ],
+                [
+                    'url' => env('REDIS_URL'),
+//                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'host' => 'redis-node-2',
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
+
+                ],
+                [
+                    'url' => env('REDIS_URL'),
+//                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'host' => 'redis-node-3',
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
+
+                ],
+            ],
+
         ],
 
     ],
